ead18c862132d7c42706400703a3a2c2fc2a263c
CHAOUCH Donia <dchaouch01@gmail.com>
2024-11-22 18:47:38 +0100

    initialializing spring boot project

---
+++ src/main/java/edu/polytech/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,13 @@
+package edu.polytech.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoApplication.class, args);
+    }
+
+}
